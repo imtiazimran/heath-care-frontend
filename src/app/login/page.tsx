adf604cece27e42697e2c989b2ab81038ef80deb
imtiazimran <imtiazimran@outlook.com>
2025-04-06 21:39:44 +0600

    Add login and registration pages with form handling using react-hook-form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,77 @@
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+ 
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
     return (
-        <div>
-          <h1>This is from LoginPage</h1>
-        </div>
+        <Container>
+         <Stack sx={{
+          height: '100dvh',
+          justifyContent: 'center',
+          alignItems: 'center',
+         }}>
+          <Box 
+              sx={{
+                maxWidth: 600,
+                width: '100%',
+                boxShadow: 1,
+                borderRadius: 1,
+                padding: 4,
+                textAlign: 'center',
+              }}>
+                   <Stack sx={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <Box>
+                    <Image src={assets.svgs.logo} alt="logo" width={50} height={50} />
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" fontWeight={600}>
+                      Login
+                    </Typography>
+                  </Box>
+                </Stack>
+                <Box>
+                 <form>
+                 <Grid container spacing={2} my={1}>
+                    <Grid item md={6}>
+                    <TextField size="small" fullWidth={true} id="outlined-basic" label="Email" type="email" variant="outlined" />
+                    </Grid>
+                    <Grid item md={6}>
+                    <TextField size="small" fullWidth={true} id="outlined-basic" label="Password" type="password" variant="outlined" />
+                    </Grid>
+                    
+
+                  </Grid>
+                  
+                  <Button sx={{my: 2}} fullWidth={true} >Login</Button>
+                 </form>
+                 <Typography my={2} component="p" fontWeight={300}>
+                    Forgot Password? <Link className="text-[#1586fd]" href="/forgot-password">Reset</Link>
+                    </Typography>
+                  <Typography component="p" fontWeight={300}>
+                     Don&apos;t have an account? <Link className="text-[#1586fd]" href="/register">Register</Link>
+                    </Typography>
+                    
+                </Box>
+          </Box>
+         </Stack>
+        </Container>
     );
 };
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,84 @@
+"use client"
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  contact: string;
+  address: string;
+}; 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
     return (
         <div>
-          <h1>This is from Register</h1>
+          <Container >
+            <Stack sx={{
+              height: '100dvh',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+              <Box 
+              sx={{
+                maxWidth: 600,
+                width: '100%',
+                boxShadow: 1,
+                borderRadius: 1,
+                padding: 4,
+                textAlign: 'center',
+              }}>
+                <Stack sx={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                  <Box>
+                    <Image src={assets.svgs.logo} alt="logo" width={50} height={50} />
+                  </Box>
+                  <Box>
+                    <Typography variant="h6" fontWeight={600}>
+                      Patient Register
+                    </Typography>
+                  </Box>
+                </Stack>
+                <Box>
+                 <form onSubmit={handleSubmit(onSubmit)}>
+                 <Grid container spacing={2} my={1}>
+                    <Grid item md={12}>
+                    <TextField {...register("name")} size="small" fullWidth={true} id="name" label="Name" type="text" variant="outlined" />
+                    </Grid>
+
+                    <Grid item md={6}>
+                    <TextField {...register("email")} size="small" fullWidth={true} id="email" label="Email" type="email" variant="outlined" />
+                    </Grid>
+                    <Grid item md={6}>
+                    <TextField {...register("password")} size="small" fullWidth={true} id="password" label="Password" type="password" variant="outlined" />
+                    </Grid>
+                    <Grid item md={6}>
+                    <TextField {...register("contact")} size="small" fullWidth={true} id="contact" label="Contact Number" type="tel" variant="outlined" />
+                    </Grid>
+                    <Grid item md={6}>
+                    <TextField {...register("address")} size="small" fullWidth={true} id="address" label="Address" type="text" variant="outlined" />
+                    </Grid>
+                  </Grid>
+                  <Button type="submit" sx={{my: 2}} fullWidth={true} >Register</Button>
+                 </form>
+                  <Typography component="p" fontWeight={300}>
+                     Do you have an account? <Link className="text-[#1586fd]" href="/login">Login</Link>
+                    </Typography>
+                </Box>
+              </Box>
+            </Stack>
+          </Container>
         </div>
     );
 };
